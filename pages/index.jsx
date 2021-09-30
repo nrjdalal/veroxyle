bdb1bec9ec8cca712afd8a273546eab33cb13f31
Neeraj Dalal <admin@nrjdalal.com>
2021-09-30 20:50:50 +0530

    added tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import Hero from 'components/home/Hero'
 import Logo_Cloud from 'components/home/Logo_Cloud'
 import Testimonials from 'components/home/Testimonials'
@@ -10,6 +12,29 @@ import Navbar from 'components/home/Navbar'
 const index = () => {
   return (
     <>
+      <Head>
+        <title>VeroXyle - Design | Develop | Market</title>
+        <meta
+          name="keywords"
+          content="digital marketing, search engine optimisation, content marketing, social media marketing, affiliate marketing, native advertising, marketing automation, email marketing, web development"
+        />
+        <meta
+          name="news_keywords"
+          content="digital marketing, search engine optimisation, content marketing, social media marketing, affiliate marketing, native advertising, marketing automation, email marketing, web development"
+        />
+        <meta
+          name="description"
+          content="We are VeroXyle and we love building great brands & digital experiences. We offer excellent design, development and marketing services."
+        />
+        <meta name="robots" content="index,follow" />
+
+        <meta name="og:site_name" content="VeroXyle" />
+        <meta name="og:title" content="VeroXyle - Design | Develop | Market" />
+        <meta
+          name="og:description"
+          content="We are VeroXyle and we love building great brands & digital experiences. We offer excellent design, development and marketing services."
+        />
+      </Head>
       <Navbar />
       <Hero />
       <Logo_Cloud />
