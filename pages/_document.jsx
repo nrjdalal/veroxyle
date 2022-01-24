2373f2be8b7148b2e92a1288ff81ab206f4a05dd
nrjdalal <nrjdalal@nrjdalal.local>
2022-01-25 01:31:12 +0530

    gsv

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,11 +10,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-SBFNG4F7X2"></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-SBFNG4F7X2');
-          </script>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-SBFNG4F7X2" strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-SBFNG4F7X2');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
